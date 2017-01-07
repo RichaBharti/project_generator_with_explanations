f34b39e78267a1332c7ee064e7cda8d307802d0c
Richa <richa.bharti@uni-wuerzburg.de>
2017-01-07 01:57:34 +0100

    project generator templates

---
+++ create_folders_and_project_generator_Markdown.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+clear
+
+# Create the folders for each projects
+
+mkdir -p analyses bin data docs
+
+# Fill your details below
+Title="Your Project Title Here"
+Author="Richa"
+Affiliation="Univeristy of Wuerzburg"
+main_email="xyz@uni-wuerzburg.de"
+Date=`date +"%F"`
+Aim="Aim of the Project"
+
+# Create the input for markdown
+cat <<EOF > ReadMe.md
+Title: $Title   
+Author: $Author   
+Affiliation: $Affiliation   
+Email: $main_email   
+Date: $Date   
+
+
+* **Aim of the project** : 
+$Aim 
+
+
+
+* **People involved** :  
+	- _Person1 <email1@uni-wuerzburg.de>_
+	- _Person2 <email2@uni-wuerzburg.de>_
+
+
+
+* **Related literature** :
+	- <http://ref1>
+	- <http://ref2>
+	- <http://ref3>
+
+
+
+* **Background information**
+
+
+* **Extra or Miscellaneos information**
+
+
+* **Notes** 
+	- Meeting (date)
+	- With whom
+	- about
+
+EOF
+#pandoc --from markdown demo.md --to org > demo.org
